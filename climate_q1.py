2d3cbd8533e4e8474a4cd18be12299decd203980
Tanish Anand <tanish.anand@griffithuni.edu.au>
2023-09-16 16:05:48 +1000

    Code added to climate_q1.py file

--- climate_q1.py
+++ climate_q1.py
@@ -1,18 +1,26 @@
-import matplotlib.pyplot as plt
-        
+import sqlite3
+
+# Connect to the climate.db database
+database_connection = sqlite3.connect('climate.db')
+#A "cursor" is used to retrieve data from the database
+cursor = database_connection.cursor()
+
+#Create empty lists that will be populated!
 years = []
-co2 = []
-temp = []
+temperatures = []
+precipitations = []
 
-plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+try:
+    cursor.execute('SELECT year, temperature, precipitation FROM climate_data')
+    rows = cursor.fetchall()
+    for row in rows:
+        years.append(row[0])
+        temperatures.append(row[1])
+        precipitations.append(row[2])
+finally:
+    #Good practice to close the database after done with it
+    database_connection.close()
 
-plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_1.png") 
+print("First 10 years:", years)
+print("First 10 temperatures:", temperatures)
+print("First 10 precipitations:", precipitations)
