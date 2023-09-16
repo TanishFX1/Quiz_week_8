3462e81a79b3f3a6a8e9cac8725f6a99c5fb3a4a
Tanish Anand <tanish.anand@griffithuni.edu.au>
2023-09-16 16:19:57 +1000

    Code added to climate_q2.py file

--- climate_q2.py
+++ climate_q2.py
@@ -1,20 +1,26 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-years = []
-co2 = []
-temp = []
+#Loading data from CSV file i nano-ed the file for this task and realised
+# in my task 1 i wrote precipitation, so i've got Year, CO2 and Temperature.
+data = pd.read_csv('climate.csv')
 
-plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+#Creating a figure for the plots (Not sure if width (15) and height (10) would be
+#too big or small
+plt.figure(figsize=(15, 10))
 
-plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
+plt.subplot(1, 2, 1)
+plt.plot(data['Year'], data['CO2'])
+plt.title("CO2 Levels Over Time")
+plt.xlabel("Year")
+plt.ylabel("CO2 Levels")
 
+plt.subplot(1, 2, 2)
+plt.plot(data['Year'], data['Temperature'])
+plt.title("Temperature Changes Over Time")
+plt.xlabel("Year")
+plt.ylabel("Temperature (C)")
+
+# Display the plots
+plt.tight_layout()
+plt.show()
